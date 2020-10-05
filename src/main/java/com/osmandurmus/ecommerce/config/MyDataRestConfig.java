c5a25b0221c062400cef7f4ea36a85fce6e8c71c
OSMAN DURMUS <osmandurmus4@gmail.com>
2020-10-05 13:21:35 +0300

    Version 1.0 commit-Veritabanındaki ürünlerin bilgileri önyüzde gösterildi.

---
+++ src/main/java/com/osmandurmus/ecommerce/config/MyDataRestConfig.java
@@ -0,0 +1,29 @@
+package com.osmandurmus.ecommerce.config;
+
+import com.osmandurmus.ecommerce.entity.Product;
+import com.osmandurmus.ecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        HttpMethod[] theUnSupportedActions={HttpMethod.PUT,HttpMethod.POST,HttpMethod.DELETE};
+
+        //disable HTTP methods for Product: PUT, POST and DELETE
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnSupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnSupportedActions));
+
+        //disable HTTP methods for ProductCategory: PUT, POST and DELETE
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnSupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnSupportedActions));
+
+    }
+}
---
+++ src/main/java/com/osmandurmus/ecommerce/dao/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.osmandurmus.ecommerce.dao;
+
+import com.osmandurmus.ecommerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("http://localhost:4200")
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
